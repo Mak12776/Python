6575d03d6d5c5541368134e45b90679892cdc3e8
Mohammad Amin <mak12776@gmail.com>
2020-01-03 07:45:18 +0330

    more

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
 import sys
-from libs.pygene import *
 
-
-node = Function('sum', ['a', 'b'], [])
-
+char_range = lambda start, stop, step = 1: map(chr, range(ord(start), ord(stop), step))
+print(list(char_range('a', 'z')))
 
